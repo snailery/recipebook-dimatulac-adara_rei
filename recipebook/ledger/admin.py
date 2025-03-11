ee431b6ddba812e5cf66c68385b17cf5c6c893db
snailery <87980202+snailery@users.noreply.github.com>
2025-03-11 20:34:09 +0800

    modified admin panel to include RecipeIngredient inline

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, RecipeIngredient, Ingredient
 
-class RecipeInline(admin.TabularInline):
-    model = Recipe
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     search_fields = ('name',)
     list_display = ('name',)
 
+    inlines = [RecipeIngredientInline]
+
     fieldsets = [
         ('Details', {
             'fields': [
-                ('name')
+                ('name'), 
             ]
         }),
     ]
 
+
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
