356a1c3557fb0975c13447dafc6884813c4e332e
snailery <87980202+snailery@users.noreply.github.com>
2025-03-11 23:45:47 +0800

    moved user models into separate accounts app

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.TextField(
-        validators=[
-            MinLengthValidator(255)
-        ]
-    )
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
