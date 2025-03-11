6e6ee991ce4a9870594f449b0346970a64183fe7
snailery <87980202+snailery@users.noreply.github.com>
2025-03-11 20:08:46 +0800

    added admin panel for recipe

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    search_fields = ('name',)
+    list_display = ('name',)
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('name')
+            ]
+        }),
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
