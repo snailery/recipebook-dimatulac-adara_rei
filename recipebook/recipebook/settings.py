18965baeefda25d49bf44d944cbd4edf0773dd17
snailery <87980202+snailery@users.noreply.github.com>
2025-03-24 13:41:28 +0800

    configured site settings to allow for image upload

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = "static/"
 LOGIN_REDIRECT_URL = '/recipes'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
