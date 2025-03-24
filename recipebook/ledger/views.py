22fcd5c9b99341a61bcf8880f729d965d940b1fc
snailery <87980202+snailery@users.noreply.github.com>
2025-03-24 15:28:25 +0800

    created views for add recipe & add recipe image

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,11 @@
-# from django.shortcuts import render
 # from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.views.generic.edit import CreateView
+
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 class RecipeListView(ListView):
     model = Recipe
@@ -11,4 +14,39 @@ class RecipeListView(ListView):
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe.html'
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = 'add_recipe.html'
+    form_class = RecipeForm
     
+    def get_success_url(self):
+        return reverse('ledger:recipes')
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = 'add_recipe.html'
+    form_class = RecipeForm
+
+    def get_success_url(self):
+        return reverse('ledger:recipes')
+
+
+def add_image(request, pk):
+    form = RecipeImageForm()
+
+    if request.method == "POST":
+        form = RecipeImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('ledger:recipe', args=[pk]))
+        else:
+            form = RecipeImageForm()
+            
+    ctx = {
+        'form': form
+    }
+
+    return render(request, 'add_image.html', ctx)
