dbd9da4d3ed5fa0eeae57336d25a48f812decc55
snailery <87980202+snailery@users.noreply.github.com>
2025-03-24 21:46:32 +0800

    fixed forgot password email backend + aesthetic changes

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
 LOGIN_REDIRECT_URL = '/recipes'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
