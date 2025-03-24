014bcecf6a07ec7628315cafadc9b713f28ae585
snailery <87980202+snailery@users.noreply.github.com>
2025-03-24 13:41:39 +0800

    added panel for RecipeImage to admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,23 +1,30 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+
+
+class RecipeImageAdmin(admin.StackedInline):
+    model = RecipeImage
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     search_fields = ('name', 'author', 'created_on', 'updated_on')
     list_display = ('name', 'author', 'created_on', 'updated_on')
 
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageAdmin]
 
     fieldsets = [
         ('Details', {
             'fields': [
-                ('name', 'author'), 
+                ('name', 'author'),
             ]
         }),
     ]
